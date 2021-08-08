72a5ccecc614b0be3409a8fa921b6c336974270c
yassinelhadedy <yassinelhadedy@hotmail.com>
2021-08-09 00:43:31 +0200

    splash screen

--- app/src/main/java/com/jumia/myapplication/ui/products/ProductDetailFragment.kt
+++ app/src/main/java/com/jumia/myapplication/ui/products/ProductDetailFragment.kt
@@ -4,22 +4,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
 import androidx.paging.ExperimentalPagingApi
-import com.jumia.myapplication.R
+import com.jumia.myapplication.databinding.FragmentProductDetailBinding
+import com.jumia.myapplication.domain.Product
+import com.jumia.myapplication.ui.util.progress.WaitingDialog
+import com.jumia.myapplication.ui.util.state.Status
 import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalPagingApi
 @AndroidEntryPoint
 class ProductDetailFragment : Fragment() {
     @ExperimentalPagingApi
     private val productViewModel: ProductViewModel by activityViewModels()
+    private val mWaitingDialog: WaitingDialog by lazy { WaitingDialog(requireActivity()) }
+    private lateinit var viewDataBinding: FragmentProductDetailBinding
 
+    @ExperimentalPagingApi
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+        viewDataBinding = FragmentProductDetailBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewmodel = productViewModel
+        }
+        return viewDataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val safeArgs: ProductDetailFragmentArgs by navArgs()
+        Toast.makeText(requireContext(),safeArgs.productId.toString(),Toast.LENGTH_LONG).show()
+        observer()
+        productViewModel.productInfo(safeArgs.productId)
+    }
+
+    private fun observer() {
+        productViewModel.productData.observe(viewLifecycleOwner, {
+            when (it?.status) {
+                Status.SUCCESS -> {
+                    mWaitingDialog.dismissDialog()
+                    if (it.data is Product) {
+                        Toast.makeText(requireContext(), "here", Toast.LENGTH_LONG).show()
+                    }
+                }
+                Status.ERROR -> {
+                    mWaitingDialog.dismissDialog()
+                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
+                }
+                Status.LOADING -> {
+                    mWaitingDialog.showDialog()
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/jumia/myapplication/ui/products/ProductListFragment.kt
+++ app/src/main/java/com/jumia/myapplication/ui/products/ProductListFragment.kt
@@ -8,9 +8,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.paging.ExperimentalPagingApi
-import com.jumia.myapplication.R
-import com.jumia.myapplication.infrastructure.dto.JumConfiguration
+import com.jumia.myapplication.databinding.FragmentProductListBinding
+import com.jumia.myapplication.domain.Product
 import com.jumia.myapplication.ui.util.progress.WaitingDialog
 import com.jumia.myapplication.ui.util.state.Status
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,41 +26,25 @@ class ProductListFragment : Fragment() {
     @ExperimentalPagingApi
     private val productViewModel: ProductViewModel by activityViewModels()
     private val mWaitingDialog: WaitingDialog by lazy { WaitingDialog(requireActivity()) }
+    private lateinit var viewDataBinding: FragmentProductListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_list, container, false)
+        viewDataBinding = FragmentProductListBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewmodel = productViewModel
+        }
+        return viewDataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        observer()
-        productViewModel.productInfo(1)
         getProducts()
     }
 
-    private fun observer() {
-        productViewModel.productData.observe(viewLifecycleOwner, {
-            when (it?.status) {
-                Status.SUCCESS -> {
-                    mWaitingDialog.dismissDialog()
-                    if (it.data is JumConfiguration) {
-                        Toast.makeText(requireContext(), "here", Toast.LENGTH_LONG).show()
-                    }
-                }
-                Status.ERROR -> {
-                    mWaitingDialog.dismissDialog()
-                    Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
-                }
-                Status.LOADING -> {
-                    mWaitingDialog.showDialog()
-                }
-            }
-        })
-    }
 
     private fun getProducts(){
         lifecycleScope.launch {
@@ -68,7 +53,16 @@ class ProductListFragment : Fragment() {
                 Toast.makeText(requireContext(), "failllled", Toast.LENGTH_LONG).show()
             }.collectLatest {
                 Toast.makeText(requireContext(), "here222222222", Toast.LENGTH_LONG).show()
+                openProductDetailFragment(2)
             }
         }
+    }
+
+    private fun openProductDetailFragment(productId: Int) {
+        val action =
+            ProductListFragmentDirections.actionProductListFragmentToProductDetailFragment(
+                productId
+            )
+        findNavController().navigate(action)
     }
 }
