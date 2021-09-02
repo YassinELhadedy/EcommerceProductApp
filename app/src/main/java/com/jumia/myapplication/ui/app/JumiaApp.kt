a7e7b6167d705daab155c8c1d38e9b7efe10a8b5
yassinelhadedy <yassinelhadedy@hotmail.com>
2021-09-02 15:02:48 +0200

    appcenter

--- app/src/main/java/com/jumia/myapplication/ui/app/JumiaApp.kt
+++ app/src/main/java/com/jumia/myapplication/ui/app/JumiaApp.kt
@@ -1,7 +1,18 @@
 package com.jumia.myapplication.ui.app
 
 import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class JumiaApp(): Application()
+class JumiaApp(): Application(){
+    override fun onCreate() {
+        super.onCreate()
+        AppCenter.start(
+           this, "4accee15-1036-41cb-ac1f-05f44c30ae07",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
