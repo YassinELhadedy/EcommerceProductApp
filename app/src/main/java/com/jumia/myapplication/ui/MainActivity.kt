17930127dbaa80cd2d5f435413413abfe21225f8
yassinelhadedy <yassinelhadedy@hotmail.com>
2021-09-02 15:12:48 +0200

    appcenter main

--- app/src/main/java/com/jumia/myapplication/ui/MainActivity.kt
+++ app/src/main/java/com/jumia/myapplication/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
 import com.jumia.myapplication.R
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 
@@ -13,5 +16,9 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_JumiaApp)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "4accee15-1036-41cb-ac1f-05f44c30ae07",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
--- app/src/main/java/com/jumia/myapplication/ui/app/JumiaApp.kt
+++ app/src/main/java/com/jumia/myapplication/ui/app/JumiaApp.kt
@@ -1,18 +1,7 @@
 package com.jumia.myapplication.ui.app
 
 import android.app.Application
-import com.microsoft.appcenter.AppCenter
-import com.microsoft.appcenter.analytics.Analytics
-import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class JumiaApp(): Application(){
-    override fun onCreate() {
-        super.onCreate()
-        AppCenter.start(
-           this, "722828e5-c9f7-4cbf-8889-6dbd6218ad49",
-            Analytics::class.java, Crashes::class.java
-        )
-    }
-}
+class JumiaApp() : Application()
